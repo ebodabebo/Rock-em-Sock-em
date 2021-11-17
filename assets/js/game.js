a606fb0bca6478ba356121c42619aa51e2a87aa4
ebodabebo <hartfield.ebony@gmail.com>
2021-11-16 21:48:39 -0600

    Initial Commit

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+fight(); 
